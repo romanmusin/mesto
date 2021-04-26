aa254e414d52171699ef914e3b729bc61b70a03a
Roman Musin <roman9m@yandex.ru>
2021-04-26 15:10:42 +0300

    all-done

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,33 @@
 let popup = document.querySelector('.popup');
 let openPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = document.querySelector('.popup__close-button');
+let popupName = document.querySelector('.popup__input_name');
+let popupText = document.querySelector('.popup__input_text');
+let profName = document.querySelector('.profile__name');
+let profText = document.querySelector('.profile__text');
+let saveBtn = document.querySelector('.popup__save');
 
 function openPopup() {
+    popupName.value = profName.textContent;
+    popupText.value = profText.textContent;
     popup.classList.add('popup_visible');
+    console.log(popupText.value);
 }
 
 function closePopup() {
+    profName.textContent = popupName.value;
+    profText.textContent = popupText.value;
     popup.classList.remove('popup_visible');
 }
 
-openPopupBtn.addEventListener('click', function() {
-    openPopup();
-});
+openPopupBtn.addEventListener('click', openPopup);
 
-closePopupBtn.addEventListener('click', function() {
-    closePopup();
-});
-
-let popupName = document.querySelector('.popup__input-name').value = document.querySelector('.profile__name').textContent;
-let popupText = document.querySelector('.popup__input-text').value = document.querySelector('.profile__text').textContent;
-let saveBtn = document.querySelector('.popup__save');
-
-
-function edit() {
-    popupName = document.querySelector('.popup__input-name').value;
-
-    document.querySelector('.profile__name').textContent = popupName;
-    
-    popupText = document.querySelector('.popup__input-text').value;
-    
-    document.querySelector('.profile__text').textContent = popupText;
-}
+closePopupBtn.addEventListener('click', closePopup);
  
- 
-saveBtn.addEventListener('click', function () {
-    edit();
-    closePopup();
-});
+saveBtn.addEventListener('click', closePopup);
 
-let like = document.querySelectorAll('.element__text-container_like').forEach(item => {
-    item.addEventListener('click', function (evt) {
-        evt.target.classList.toggle('element__text-container_like-active');
-    })
-  });
-
-
-
+//let like = document.querySelectorAll('.element__like').forEach(item => {
+//    item.addEventListener('click', function (evt) {
+//        evt.target.classList.toggle('element__like-active');
+//    })
+//  });
