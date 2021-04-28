773cad4485ae5ce5f9cb7e245785bf8117be77e6
Roman Musin <roman9m@yandex.ru>
2021-04-28 10:51:45 +0300

    all-done

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,40 @@
 let popup = document.querySelector('.popup');
+
 let openPopupBtn = document.querySelector('.profile__edit-button');
+
 let closePopupBtn = document.querySelector('.popup__close-button');
-let popupName = document.querySelector('.popup__input_name');
-let popupText = document.querySelector('.popup__input_text');
+
+let popupName = document.querySelector('.popup__input_type_name');
+
+let popupText = document.querySelector('.popup__input_type_text');
+
 let profName = document.querySelector('.profile__name');
+
 let profText = document.querySelector('.profile__text');
+
 let saveBtn = document.querySelector('.popup__save');
 
 function openPopup() {
     popupName.value = profName.textContent;
     popupText.value = profText.textContent;
     popup.classList.add('popup_visible');
-    console.log(popupText.value);
 }
 
 function closePopup() {
+    popup.classList.remove('popup_visible');
+}
+
+function edit() {
     profName.textContent = popupName.value;
     profText.textContent = popupText.value;
-    popup.classList.remove('popup_visible');
+    closePopup();
 }
 
 openPopupBtn.addEventListener('click', openPopup);
 
 closePopupBtn.addEventListener('click', closePopup);
  
-saveBtn.addEventListener('click', closePopup);
+saveBtn.addEventListener('click', edit);
 
 //let like = document.querySelectorAll('.element__like').forEach(item => {
 //    item.addEventListener('click', function (evt) {
