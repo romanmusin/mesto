1991ca8ef72fcfec0f64984876e972ceba3c1803
Roman Musin <roman9m@yandex.ru>
2021-06-08 08:57:11 +0300

    complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
+
+/*Здравствуйте. В прошлый раз в консоли у меня не было никаких ошибок, в этот раз тоже нет, если будут,
+    прикрепите пожалуйста скрин.
+    Также в макете нет информации по адаптивности страницы между 1280px и 320px. В задании в практике
+    об этом тоже ничего не сказано, пожтому садаптировал интуитивно.*/
+
+    
 const popupEditProfile = document.querySelector('.popup');
 const popupCard = document.querySelector('.popup_add-card');
 const openPopupBtn = document.querySelector('.profile__edit-button');
@@ -9,34 +16,12 @@ const profName = document.querySelector('.profile__name');
 const profText = document.querySelector('.profile__text');
 const popupForm = document.querySelector('form');
 
-function openPopup() {
-    popupName.value = profName.textContent;
-    popupText.value = profText.textContent;
-    popupEditProfile.classList.add('popup_visible');
-}
-
-function openPopupCard() {
-    popupCard.classList.add('popup_add-card_visible');
-}
-
-function closePopup() {
-    popupEditProfile.classList.remove('popup_visible');
-}
-
 function edit (evt) {
     evt.preventDefault();
     profName.textContent = popupName.value;
     profText.textContent = popupText.value;
-    closePopup();
-}
-
-openPopupBtn.addEventListener('click', openPopup);
-
-plusButton.addEventListener('click', openPopupCard);
-
-closePopupBtn.addEventListener('click', closePopup);
-
-popupCard.querySelector('.popup__close-button').addEventListener('click', closePopupCard);
+    popupClose(popupEditProfile, 'popup_visible');
+};
 
 popupForm.addEventListener('submit', edit);
 
@@ -52,8 +37,6 @@ const popupImage = document.querySelector('.popup__image-pic');
 const popupImageText = document.querySelector('.popup__image-text');
 
 function createCards(title, image) {
-    
-
     elName.textContent = title;
     elImage.src = image;
     elImage.alt = title;
@@ -71,10 +54,11 @@ function createCards(title, image) {
     cardItem.querySelector('.element__image').addEventListener('click', function(evt) {
             popupImage.src = evt.target.closest('.element__image').src;
             popupImageText.textContent = evt.target.closest('.element__image').alt;
-            popupImg.classList.add('popup_image_visible');
+            popupOpen(popupImg, 'popup_image_visible');
         });
     return(cardItem);
 };
+
 
 initialCards.forEach(function(item) {
     elName.textContent = item.name;
@@ -83,18 +67,13 @@ initialCards.forEach(function(item) {
     elements.prepend(createCards(item.name, item.link));
 });
 
-function closePopupCard() {
-    popupCard.classList.remove('popup_add-card_visible');
-    document.querySelector('.popup_add-card').querySelector('form').reset();
-}
-
 function newCard(evt) {
     evt.preventDefault();
     elName.textContent = userTitle.value;
     elImage.src = userLink.value;
     elImage.alt = userTitle.value;
     elements.prepend(createCards(userTitle.value, userLink.value));
-    closePopupCard(evt);
+    closeForm(evt);
 };
 
 const cardForm = document.querySelector('.popup_add-card').querySelector('form');
@@ -104,8 +83,35 @@ const closePicBtn = document.querySelector('.popup__image-close');
 
 const popupImg = document.querySelector('.popup_image');
 
-function closePic() {
-    popupImg.classList.remove('popup_image_visible');
-};
+function popupOpen(item, visible) {
+    item.classList.add(visible)
+}
 
-closePicBtn.addEventListener('click', closePic);
+function popupClose(item, visible) {
+    item.classList.remove(visible)
+}
+
+function closeForm() {
+    popupClose(popupCard, 'popup_add-card_visible');
+    document.querySelector('.popup_add-card').querySelector('form').reset();
+}
+
+openPopupBtn.addEventListener('click', function() {
+    popupName.value = profName.textContent;
+    popupText.value = profText.textContent;
+    popupOpen(popupEditProfile, 'popup_visible')
+});
+
+closePopupBtn.addEventListener('click', function() {
+    popupClose(popupEditProfile, 'popup_visible')
+});
+
+plusButton.addEventListener('click', function() {
+    popupOpen(popupCard, 'popup_add-card_visible');
+});
+
+popupCard.querySelector('.popup__close-button').addEventListener('click', closeForm);
+
+closePicBtn.addEventListener('click', function() {
+    popupClose(popupImg, 'popup_image_visible');
+});
